e430484d67e496e2d3c66b43fdf7b2efc3a96cd2
xqly-stack <oleg.ionov@tutanota.de>
2022-12-12 18:06:54 +0000

    Added output method to count the passes inside the loop

--- bubblesort/BubblesortMuster.java
+++ bubblesort/BubblesortMuster.java
@@ -2,12 +2,11 @@ package bubblesort;
 
 import java.util.Scanner;
 
-public class BubblesortMuster {
 
-    Scanner scan = new Scanner(System.in);
-    public static void main(String[] args) {  
+public class BubblesortMuster {
+    
+  public static void main(String[] args) {  
     // Deklaration der Variablen
-    Scanner scan = new Scanner(System.in);
     double summe=0;
     double durchschnitt=0;
     double max=0;
@@ -15,23 +14,20 @@ public class BubblesortMuster {
     String wahl;
     int zwischenspeicher;
     
-    // Deklaration des Arrays / Definition/Eingabe der Länge
-    System.out.println("Anzahl Elemente eingeben:\t");
-    int anzahlElemente = scan.nextInt();
+    Scanner eingabe = new Scanner(System.in);
+    // Deklaration des Arrays / Definition/Eingabe der L?ge
+    System.out.printf("\nAnzahlElemente eingeben:\t");
+    int anzahlElemente = eingabe.nextInt();
     int[] zahlen=new int[anzahlElemente];
-          
-    // Das Array wird gefühlt mit Zahlen des Users 
+    
+    // Das Array wird gef?lt mit Zahlen des Users 
     zahlen = befuelleArray(zahlen);
     
-    for (int i=0;i<zahlen.length;i++ ) {
-      zahlen[i]=eingabe.getInt((i+1)+ ".Zahl eingeben:\t");
-    } 
-                
     // Addiert alle Zahlen im Array
     for (int i=0;i<zahlen.length;i++ ) {
       summe=summe +zahlen[i];
     } 
-         
+    
     // Sucht das Minimum und das Maximum im Array
     for (int i=0;i<zahlen.length;i++ ) {  
       if (i==0) {
@@ -48,23 +44,27 @@ public class BubblesortMuster {
       } 
     } 
     
-    // Menüauswahl
+    // Men?uswahl
     System.out.println("Sortierreihnfolge:\t");
     System.out.println("a-aufsteigend");
     System.out.println("b-absteigend");
     
     do {
-      wahl = eingabe.getString("Ihre Wahl:\t");  
+      System.out.printf("\nIhre Wahl:\t");
+      wahl = eingabe.nextLine();  
     } while (wahl.equals("a") && wahl.equals ("b")); 
     
     // Gibt das Array auf der Konsole aus
     for (int i=0;i<zahlen.length ;i++ ) {
       System.out.print(zahlen[i]+"\t");
     }  
-    
+   
+    int sum = 0;
     // Sortieren nach Bubblesort
     for (int i =0;i<zahlen.length ;i++ ) {
+      sum+=1;
       for (int s=1;s<zahlen.length-i ;s++ ) {
+        sum+=1;
         if (wahl.equals("a")) {
           if (zahlen[s]> zahlen[s-1]) {
             // tauscht zwei Zahlen im Array
@@ -84,27 +84,36 @@ public class BubblesortMuster {
         } // end of if-else    
       } // end of for    
     } // end of for
-    
+   
+    System.out.println("\nSchleifendurchgänge: "+ sum);
     // Gibt das Array auf der Konsole aus
     for (int i=0;i<zahlen.length ;i++ ) {
       System.out.print(zahlen[i] + "\t");
     } 
     
     // Geb alle Ergebnisse aus  
-    durchschnitt= summe/zahlen.length;
-    System.out.println("Die Summe :\t"+summe);
-    System.out.println("Der Durchschnitt :\t"+durchschnitt);
-    System.out.println(" Die Maximale wert:\t"+max);
-    System.out.println("Die Minimale Wert:\t"+min);
-    
-  }
-
+    ausgabeAlles(summe, max, min, zahlen);
+    eingabe.close();
+  } 
+  
   public static int[] befuelleArray(int[] zahlen) {
-    
+    Scanner eingabe = new Scanner(System.in);
     for (int i=0;i<zahlen.length;i++ ) {
-      zahlen[i]=scan.nextInt((i+1)+ ".Zahl eingeben:\t");
-    
+        System.out.println((i+1) + ".Zahl eingeben:\t");      
+      zahlen[i]=eingabe.nextInt();
+    }
+    eingabe.close();
     return zahlen;
   }
-
+  
+  public static void ausgabeAlles(double a, double b, double c, int[] d){
+    double durchschnitt= a/d.length;
+    System.out.println();
+    System.out.println("Die Summe :\t"+a);
+    System.out.println("Der Durchschnitt :\t"+durchschnitt);
+    System.out.println("Die Maximale wert:\t"+b);
+    System.out.println("Die Minimale Wert:\t"+c);
+  }
+    
 }
+
